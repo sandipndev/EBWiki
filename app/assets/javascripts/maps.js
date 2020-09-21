570f396d0b963fcb42752c574372e4a166dcff73
Mark Nyon <mark@grandkru.com>
2020-09-21 07:51:39 +0000

    Converted Cases JSON to HERE Markers #3652
    - app/assets/javascripts/maps.js - Added mapCoordinates() function to parse
      cases string and convert that to Clustering Datapoints that HERE maps
      can use
    
    - app/views/layouts/application.html.erb - Added HERE Maps UI librares
      to add zoom and default controls to the map

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -17,14 +17,24 @@ document.addEventListener("DOMContentLoaded", function() {
     );
 
 	let coordinates = window.CASES;
+	let mapCoordinates = (coordinateString) => {
+		let mappedCoordinates = coordinates.slice(2, -2).split("], [");
+		return mappedCoordinates.map(coord => new H.clustering.DataPoint(
+			coord.split(", ")[0],
+			coord.split(", ")[1]
+		));
+	}
 
-	let createdObjects = coordinates.map(coord => new H.clustering.DataPoint(coord[0], coord[1]));
+	let createdObjects = mapCoordinates(coordinates);
 	let clusteredDataProvider = new H.clustering.Provider(createdObjects);
 	// Create a layer that includes the data provider and its data points: 
 	let layer = new H.map.layer.ObjectLayer(clusteredDataProvider);
 	
 	// Add the layer to the map:
 	map.addLayer(layer);
+	
+	// Create a default UI instance:
+	let ui = H.ui.UI.createDefault(map, defaultLayers);
 	
 	window.addEventListener('resize', () => map.getViewPort().resize());
 	let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
